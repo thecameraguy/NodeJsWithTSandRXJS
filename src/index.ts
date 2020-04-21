e3f4c768427f9947df845c624a29f0e2f21e2b8c
Jin Choi <jinwoo.choi24@gmail.com>
2020-04-20 22:48:27 -0400

    WIP MongoDB initialization
    * Will factor out into DBManager

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { DAOBundle } from './models/dataAccessObjects/DAOBundle';
 import { MongooseCityDAO } from './models/dataAccessObjects/mongooseCityDAO';
 import { MongooseBuildingDAO } from './models/dataAccessObjects/mongooseBuildingDAO';
 
+const Mongoose = require('mongoose');
+const mongoDB = 'theDBURL';
 const express = require('express');
 const app = express();
 
@@ -20,5 +22,20 @@ export class Main {
         routesManager.buildRoutes(app);
     }
 }
+
+// TODO take out the DB connection to its own class. - DB Manager.
+Mongoose.Promise = global.Promise;
+const db = Mongoose.connection;
+
+db.on('open', function () {
+    console.log('connected to mongodb');
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}!`);
+    })
+});
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
+
+Mongoose.connect(mongoDB);
 
 Main.main();
