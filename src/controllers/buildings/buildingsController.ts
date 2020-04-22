4c03fabf50218d0e858f1798a3d53c4f7a2ea538
Jin Choi <jinwoo.choi24@gmail.com>
2020-04-22 14:11:24 -0400

    Cleanup

--- src/controllers/buildings/buildingsController.ts
+++ src/controllers/buildings/buildingsController.ts
@@ -9,9 +9,7 @@ import { IBuildingDAO } from '../../models/dataAccessObjects/buildingDAO';
 // TODO Singleton. We could make an InjectorService that takes care of dependency injection..?
 
 /**
- * Express Routable controller - knows how to handle request and response from express.js
- * // TODO See if ExpressController needs to be factored out into a common class. Maybe BuildingsController can
- *              hold an express delegate that knows how to handle req and res.
+ * Builginds routable controller - knows how to handle web server agnostic requests.
  */
 export class BuildingsController {
     private m_cityDAO: ICityDAO;
